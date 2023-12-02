3b2667e0c3e27c4f6f4f00713805d59b450298e7
MAT-2 <mpcasey30@gmail.com>
2023-12-01 21:18:28 -0600

    Added some styling to About Me page. Added image.

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Refer to License
 
 - UT Austin Coding Bootcamp
 - https://mdbootstrap.com/docs/standard/navigation/navbar/#
+- https://create-react-app.dev/docs/adding-images-fonts-and-files/
+- Image Icon: https://www.iconpacks.net/free-icon/user-3296.html
+-
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,36 @@
-export default function About() {
-  return <h1>About Me</h1>;
+const styles = {
+  heading: { fontSize: "3rem", margin: "10px" },
+  content: { fontSize: "25px", margin: "10px" },
+  // body: {
+  //   background: "#00b2f6",
+  // },
+  image: { height: "100px", width: "100px", margin: "10px" },
+};
+
+function About() {
+  return (
+    <div className="div" style={styles.body}>
+      <h1 style={styles.heading}>About Me</h1>
+      <img
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        style={styles.image}
+      ></img>
+      <p style={styles.content}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
+        provident ipsam eos saepe adipisci repudiandae ipsum facere nisi
+        deserunt quibusdam, totam possimus soluta ullam hic ut amet sequi
+        praesentium obcaecati laudantium voluptatum magnam dolor voluptates
+        aspernatur! Aliquid nihil neque magnam, veniam vero labore impedit quis!
+        Accusantium exercitationem accusamus deserunt impedit eligendi sint
+        consequuntur quam nesciunt iure perferendis. Dolore sequi ipsam, ratione
+        voluptatibus cum excepturi. Aperiam inventore possimus commodi.
+        Cupiditate eum tenetur laboriosam odit quis quaerat possimus corrupti
+        ipsum quo eius recusandae necessitatibus numquam sed, dolorum sequi
+        dignissimos illum omnis. Sunt consequatur nisi dignissimos at maxime
+        numquam consectetur pariatur aut explicabo.
+      </p>
+    </div>
+  );
 }
+
+export default About;
